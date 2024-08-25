ca7bac5658eb87e45d0c2de247c20fbfe7bf946e
Ari785 <u1902049@student.cuet.ac.bd>
2024-08-25 19:32:33 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ with st.sidebar:
                             'Parkinson Prediction',
                            'Breast Cancer Prediction'],
                            menu_icon='hospital-fill',
-                           icons=['activity', 'heart', 'person','Virus'],
+                           icons=['activity', 'heart', 'person','virus'],
                            default_index=0)
 
 # Helper function to display the result
